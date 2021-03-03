f64c03e1ae2ea25c019e0af980249cbc70780644
Jayanthinisha .alphind <jayanthinishabtech@gmail.com>
2021-03-03 14:30:39 +0530

    test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPlus';
+
+  storeInLocalStorage(key: string, value: string) {
+    localStorage.setItem(key, value);
+}
+
+// Getting from Local Storage
+getFromLocalStorage(key: string): string {
+    let a;
+    const value = localStorage.getItem(key) ? localStorage.getItem(key) || '{}' : '{}';
+    return value;
+}
+
+// Clearing Local Storage
+clearLocalStorage() {
+    localStorage.clear();
+}
+
+// Storing in Session Storage
+storeInSessionStorage(key: string, value: string) {
+    sessionStorage.setItem(key, value);
+}
+
+// Getting from Session Storage
+  getFromSessionStorage = (key: string) => {
+    const value = sessionStorage.getItem(key) ? sessionStorage.getItem(key) : undefined;
+      return value;
+}
+
+// Clearing Session Storage
+clearSessionStorage() {
+    sessionStorage.clear();
+}
+
 }
